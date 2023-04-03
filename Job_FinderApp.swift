eacab43a7e0310333b0c3e620448767bb1a94471
Dinith <codse202f060@student.nibm.lk>
2023-04-03 20:00:21 +0530

    Project created

---
+++ Job_FinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Job_FinderApp.swift
+//  Job Finder
+//
+//  Created by Dinith Hasaranga on 2023-04-03.
+//
+
+import SwiftUI
+
+@main
+struct Job_FinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
